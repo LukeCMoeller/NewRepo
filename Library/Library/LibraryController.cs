b8a043e9b30773b641b49150f8eadbe88ba003df
LukeCMoeller <lcmoeller@ksu.edu>
2023-10-02 23:21:59 -0500

    i did some stuff

--- Library/Library/LibraryController.cs
+++ Library/Library/LibraryController.cs
@@ -8,13 +8,42 @@ namespace Library
 {
     public class LibraryController
     {
-        private LibraryModel lModel;
-        private LibraryView lView;
-        private LibraryDelegate lDelegate; //i got no idea how to do these
-        public LibraryController(LibraryModel LM, LibraryView LV)
+        private List<BookModel> _books = new List<BookModel>();
+        public BookModel currentBook;
+        public LibraryController()
         {
-            lModel = LM;
-            lView = LV;
+            string filePath = Path.GetFullPath("Library.txt");
+            if (File.Exists(filePath))
+            {
+                StreamReader sr = new StreamReader(filePath);
+                while (!sr.EndOfStream)
+                {
+                    string[] Books = sr.ReadLine().Split(' ');
+                    _books.Add(new BookModel(Books[0], Books[1], Books[2]));
+                }
+                sr.Close();
+            }
+        }
+        public void addBookmark(int b)
+        {
+            _books[_books.IndexOf(currentBook)].addBookmark(b); //grabs teh current book and adds a bookmark to that
+        }
+        public void removeBookmark(int b)
+        {
+            _books[_books.IndexOf(currentBook)].RemoveBookmark(b); //grabs teh current book and removes a bookmark
+ 
+        }
+        public void addBook(BookModel Book)
+        {
+            _books.Add(Book);
+        }
+        public void removeBook(BookModel Book)
+        {
+            _books.Remove(Book);
+        }
+        public void synchronize()
+        {
+            //doing know what this does
         }
 
     }
